3bcb73b384bc206c4ba8eede1b22ee757701230b
Dodi488 <161328660+Dodi488@users.noreply.github.com>
2024-11-22 09:52:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -30,28 +30,6 @@ class social:
                     x.append(self.users[j])  
             print(f"{user}'s friends: {x}")
 
-    def mutual_friends(self,user1,user2):
-        if user1 in self.users and user2 in self.users:
-            i=self.users.index(user1)
-            j=self.users.index(user2)
-
-            mutual=[self.users[k] for k in range(len(self.users)) if self.friends[i][k]==1 and self.friends[j][k]==1]
-            print(f"Mutual friends between '{user1}' and '{user2}': {mutual}")
-
-    def recommend(self,user):
-        if user in self.users:
-            i=self.users.index(user)
-            reco=[]
-
-            for j,is_friend in enumerate(self.friends[i]):
-                if is_friend==0 and i!=j:
-                    cout=sum(self.friends[i][j] and self.friends[j][k] for k in range(len(self.users)))
-                    if cout>0:
-                        reco.append((self.users[j],cout))
-
-    def show_all(self):
-        print("".join(self.users))
-
     def show(self):
         print("  "+"  ".join(self.users))
         for i,n in enumerate(self.friends):
